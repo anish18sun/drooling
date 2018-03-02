0b6f291f6c94a987489e54a6f4f8d58a2ce67269
anish.singh <anish.singh@dailyhunt.in>
2018-03-02 10:26:33 +0530

    Initial Commit

---
+++ src/main/java/com/drools/project/KnowledgeSessionHelper.java
@@ -0,0 +1,13 @@
+package com.drools.project;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.StatelessKieSession;
+
+public class KnowledgeSessionHelper {
+
+  public static KieContainer getRuleBase() {
+    KieServices
+  }
+}
---
+++ src/main/java/com/drools/project/RuleEngine.java
@@ -0,0 +1,11 @@
+package com.drools.project;
+
+public class RuleEngine {
+
+  public static void main(String[] args) {
+
+    System.out.println("Starting Rule Engine...");
+
+
+  }
+}
